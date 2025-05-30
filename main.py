eb9124d113e90b4dcc954c24b854af7bc06c74df
kanchan sharma <kanchan4596@gmail.com>
2025-05-30 19:19:42 +0530

    removed extra comments

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 
 def fetch_jobs(keyword):
     url = f"https://weworkremotely.com/remote-jobs/search?term={keyword}"
-    url = f"https://weworkremotely.com/remote-jobs/search?term={keyword}"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                       "AppleWebKit/537.36 (KHTML, like Gecko) "
